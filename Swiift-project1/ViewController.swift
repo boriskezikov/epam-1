2c13fcae38d9761df41a2857162d5287b3714909
Kezikov Boris <boris200898@icloud.com>
2020-01-23 09:46:40 +0300

    Initial Commit

---
+++ Swiift-project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swiift-project1
+//
+//  Created by Kezikov Boris  on 23.01.2020.
+//  Copyright © 2020 epam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
